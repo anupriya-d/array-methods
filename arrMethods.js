e3e3a2a3d73275dfee36b2554581ad21a2992fbf
Anupriya Dayaratne <anupriya.dayaratne@gmail.com>
2024-05-22 14:27:33 +1200

    more method added

--- arrMethods.js
+++ arrMethods.js
@@ -17,3 +17,55 @@ const websiteObj = [{name:'google',url:'www.google.com'},{name:'facebook',url:'w
 
 
 websiteObj.forEach((website)=>console.log(website.name + ' is at ' +website.url));
+
+//Array.prototype.filter
+
+const words = ['hello','morning','bye','welcome'];
+
+const threeLetterWords = words.filter(word => word.length == 3);
+
+console.log(threeLetterWords);
+
+//filter even numbers
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+const evenNums = numbers.filter(number => number%2 ===0);
+
+console.log(evenNums)
+
+
+
+//Array.prototype.map
+
+
+const doubledNums = numbers.map(x => x*2);
+
+console.log(doubledNums);
+
+
+//chain method;
+
+const squreAndTriple = numbers
+                        .map(number => Math.sqrt(number))
+                        .map(sqrt => sqrt*3);
+
+console.log(squreAndTriple);
+
+
+//reduce
+const initailVal = 0;
+
+const sumNums = numbers.reduce(function(prevVal,currentVal){
+
+    return prevVal+currentVal;
+
+},initailVal)
+
+
+console.log(sumNums)
+
+//using arrow function
+const sumNum1 = numbers.reduce((prev,curr)=> prev+curr,0);
+
+console.log(sumNum1);
